235811dff71a798e8805f3d4b484b34936363b30
projectperfect <jprfct@protonmail.com>
2022-09-10 12:49:19 -0500

    mini project completed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Game values
 let min = 1,
     max = 10,
-    winningNum = 3,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 // UI Elements
@@ -23,6 +23,13 @@ const game = document.querySelector('#game'),
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Play again event listener
+game.addEventListener('mousedown', function(e) {
+    if(e.target.className === 'play-again') {
+        window.location.reload();
+    }
+});
 
 // Listen for guess
 guessBtn.addEventListener('click', function() {
@@ -88,8 +95,18 @@ function gameOver(won, msg) {
     message.style.color = color;
     // Set message
     setMessage(msg);
+
+    // Play again
+    guessBtn.value = 'Play Again';
+    guessBtn.className += 'play-again';
 } 
 
+// Get winning Number 
+function getRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min+1) + min);
+}
+
+// Set Message
 function setMessage(msg, color) {
     message.style.color = color;
     message.textContent = msg;
